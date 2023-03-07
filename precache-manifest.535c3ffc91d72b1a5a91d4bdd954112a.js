5f611b590ad3f71e1e1358eba49673b9a2e41175
verma888 <sevtagourav@gmail.com>
2023-03-08 00:39:37 +0530

    Updates

---
+++ precache-manifest.535c3ffc91d72b1a5a91d4bdd954112a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "73a4e2dc8b405104f07e",
+    "url": "/static/css/main.cf087c73.chunk.css"
+  },
+  {
+    "revision": "73a4e2dc8b405104f07e",
+    "url": "/static/js/main.73a4e2dc.chunk.js"
+  },
+  {
+    "revision": "f09d6da400d0c14a7f1d",
+    "url": "/static/js/1.f09d6da4.chunk.js"
+  },
+  {
+    "revision": "674206ab8ea0e6c3391e",
+    "url": "/static/js/2.674206ab.chunk.js"
+  },
+  {
+    "revision": "0e85d9ea22f618c5f874",
+    "url": "/static/js/runtime~main.0e85d9ea.js"
+  },
+  {
+    "revision": "02894839e98bbebcae879aa849a80809",
+    "url": "/index.html"
+  }
+];
